2cd93477a105a57af0eb036d33193a37952ff9ac
algoadmin1 <algoinvestorr@gmail.com>
2023-12-10 15:24:11 -0800

    added json key: "entryType" for day and intraday-generated trades

--- tradesqueued.php
+++ tradesqueued.php
@@ -49,55 +49,61 @@ $rnd = rand(1,100);
 
 
 if($rnd<10){
-    $trade0 = array("tradeId"=>"trade99", "date"=>"2023-12-09T09:22:54", "symbol"=>"nil",
+    $trade0 = array("tradeId"=>"trade99", "date"=>"2023-12-09T09:22:54", "symbol"=>"nil","entryType"=>"intraday",
             "tradeCmd"=>"killBot","price"=>"0.00","tradeSize"=>"0","dur"=>"nop","signalType"=>"stopTrading",
             "reqTimestamp"=>$uTimestamp);
     echo json_encode($trade0);
    
+}else if($rnd<17){
+    $trade0 = array("tradeId"=>"trade19", "date"=>"2023-12-13", "symbol"=>"SNAP","entryType"=>"intraday",
+           "tradeCmd"=>"sellIronCondor","price"=>"54.35","wings"=>["340","350","490","500"],"tradeSize"=>"5","dur"=>"day","signalType"=>"neutralMkt",
+           "reqTimestamp"=>$uTimestamp);
+   echo json_encode($trade0);
+    
 }else if($rnd<25){
-    $trade0 = array("tradeId"=>"trade11", "date"=>"2023-12-13T13:03:54", "symbol"=>"NFLX",
+    $trade0 = array("tradeId"=>"trade11", "date"=>"2023-12-13T13:03:54", "symbol"=>"NFLX","entryType"=>"day",
            "tradeCmd"=>"sellIronCondor","price"=>"54.35","wings"=>["340","350","490","500"],"tradeSize"=>"5","dur"=>"day","signalType"=>"neutralMkt",
            "reqTimestamp"=>$uTimestamp);
    echo json_encode($trade0);
     
 }else if($rnd<35){
-    $trade0 = array("tradeId"=>"trade12", "date"=>"2023-12-13T13:03:54", "symbol"=>"NFLX",
+    $trade0 = array("tradeId"=>"trade12", "date"=>"2023-12-13T13:03:54", "symbol"=>"NFLX","entryType"=>"intraday",
            "tradeCmd"=>"sellCallCreditSpread","price"=>"485.50","wings"=>["490","500"],"tradeSize"=>"13","dur"=>"day","signalType"=>"bearishMkt",
            "reqTimestamp"=>$uTimestamp);
    echo json_encode($trade0);
     
 }else if($rnd<45){
-    $trade0 = array("tradeId"=>"trade13", "date"=>"2023-12-13T13:03:54", "symbol"=>"AAPL",
+    $trade0 = array("tradeId"=>"trade13", "date"=>"2023-12-13T13:03:54", "symbol"=>"AAPL","entryType"=>"intraday",
            "tradeCmd"=>"sellPutCreditSpread","price"=>"194.35","wings"=>["160","170"],"tradeSize"=>"17","dur"=>"day","signalType"=>"bullishMkt",
            "reqTimestamp"=>$uTimestamp);
    echo json_encode($trade0);
     
 }else if($rnd<55){
-    $trade0 = array("tradeId"=>"trade5", "date"=>"2023-12-08T09:22:54", "symbol"=>"ESTY",
+    $trade0 = array("tradeId"=>"trade5", "date"=>"2023-12-08T09:22:54", "symbol"=>"ESTY","entryType"=>"day",
            "tradeCmd"=>"buyStop","price"=>"54.35","tradeSize"=>"400","dur"=>"gtc","signalType"=>"buySignal",
            "reqTimestamp"=>$uTimestamp);
    echo json_encode($trade0);
     
 }else if($rnd<65){
-    $trade0 = array("tradeId"=>"trade3", "date"=>"2023-12-09T09:22:54", "symbol"=>"GS240315C230",
+    $trade0 = array("tradeId"=>"trade3", "date"=>"2023-12-09T09:22:54", "symbol"=>"GS240315C230","entryType"=>"day",
             "tradeCmd"=>"buyToOpen","price"=>"34.50","tradeSize"=>"4","dur"=>"day","signalType"=>"buySignal",
             "reqTimestamp"=>$uTimestamp);
     echo json_encode($trade0);
    
 }else if($rnd<75){
-    $trade0 = array("tradeId"=>"trade13", "date"=>"2023-12-14T13:03:54", "symbol"=>"AAPL",
+    $trade0 = array("tradeId"=>"trade13", "date"=>"2023-12-14T13:03:54", "symbol"=>"AAPL","entryType"=>"intraday",
            "tradeCmd"=>"buyButterfly","price"=>"194.35","wings"=>["192.50","195","197.50"],"tradeSize"=>"6","dur"=>"day","signalType"=>"neutralMkt",
            "reqTimestamp"=>$uTimestamp);
    echo json_encode($trade0);
     
 }else if($rnd<90){
-    $trade0 = array("tradeId"=>"trade2", "date"=>"2023-12-10T12:21:43", "symbol"=>"AMZN240315C130",
+    $trade0 = array("tradeId"=>"trade2", "date"=>"2023-12-10T12:21:43", "symbol"=>"AMZN240315C130","entryType"=>"intraday",
             "tradeCmd"=>"buyToOpen","price"=>"14.50","tradeSize"=>"12","dur"=>"day","signalType"=>"buySignal",
             "reqTimestamp"=>$uTimestamp);
     echo json_encode($trade0);
    
 }else{
-    $trade0 = array("tradeId"=>"trade1", "date"=>"2023-12-09T10:21:43", "symbol"=>"TSLA240315C230",
+    $trade0 = array("tradeId"=>"trade1", "date"=>"2023-12-09T10:21:43", "symbol"=>"TSLA240315C230","entryType"=>"intraday",
             "tradeCmd"=>"buyToOpen","price"=>"24.50","tradeSize"=>"5","dur"=>"day","signalType"=>"buySignal",
             "reqTimestamp"=>$uTimestamp);
     echo json_encode($trade0);
