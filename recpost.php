f245b7716ae5dc65ef2fed513516db026d9a7952
algoadmin1 <algoinvestorr@gmail.com>
2023-12-14 03:59:57 -0800

    Create recpost.php

---
+++ recpost.php
@@ -0,0 +1,13 @@
+// recpost.php
+<?php
+echo "recpost.php is running...";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // collect value of input field
+  $name = $_POST['fname'];
+  if (empty($name)) {
+    echo "Name is empty";
+  } else {
+    echo $name;
+  }
+}
+?>
