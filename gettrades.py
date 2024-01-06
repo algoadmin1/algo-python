0c23104a9cd507e8dfba34f87577b70c4ffb1175
algoadmin1 <algoinvestorr@gmail.com>
2024-01-05 17:59:46 -0800

    Update gettrades.py

--- gettrades.py
+++ gettrades.py
@@ -123,6 +123,52 @@ def StringParts(input_str,char0):
 # ] todaysTrades[ 22 ] =  RAWTRADE,483,2024-01-04,1215,SELL,SPY,atLimit,470.56,7,-0.0000%,-0,560|540|395|375,
 # ] todaysTrades[ 23 ] =  RAWTRADE,484,2024-01-04,1215,SELL,QQQ,atLimit,399.06,7,-0.3004%,-1.2,475|455|335|315,
 
+
+# 3) at 1600 BUY AMZN atLimit $144.98 count=4 $1.7 or 1.1702% above S1
+# 4) at 1600 BUY AMD atLimit $138.23 count=5 $4.1 or 2.9661% above S1
+# 5) at 1600 BUY SPY atLimit $467.15 count=5 $1.24 or 0.2654% above S1
+# 6) at 1600 BUY QQQ atLimit $395.98 count=5 $0.97 or 0.2450% above S1
+# 7) at 1545 BUY GS atLimit $386.68 count=4 $5.78 or 1.4948% above S1
+# 8) at 1500 SELL ROKU atLimit $90.28 count=4 $0.38 or 0.4234% above R1
+# (ini.10)  ROKU SELL<<===== above R1  Trade: ROKU LONG_PUTS
+# 9) at 1500 SELL AMZN atLimit $145.63 count=5 $-0.99 or -0.6783% below R1
+# 10) at 1500 SELL META atLimit $351.92 count=4 $2.89 or 0.8218% above R1
+# (ini.2)  META SELL<<===== above R1  Trade: META CREDIT_CALL_SPREAD
+# 11) at 1445 SELL SPY atLimit $468.46 count=4 $-1.36 or -0.2894% below R1
+# 12) at 1445 SELL AAPL atLimit $182.03 count=4 $-1.02 or -0.5597% below R1
+# 13) at 1445 SELL TSLA atLimit $238.73 count=6 $-2.47 or -1.0342% below R1
+# 14) at 1445 SELL GS atLimit $387.23 count=4 $1.19 or 0.3065% above R1
+# 15) at 1400 BUY AMD atLimit $139.43 count=8 $5.31 or 3.8059% above S1
+# 16) at 1400 BUY QQQ atLimit $397.4 count=9 $2.39 or 0.6014% above S1
+# 17) at 1400 BUY MSFT atLimit $368.8 count=9 $3.09 or 0.8378% above S1
+# 18) at 1345 BUY SPY atLimit $467.47 count=8 $1.57 or 0.3351% above S1
+# 19) at 1345 BUY AMZN atLimit $145.28 count=9 $1.99 or 1.3721% above S1
+# 20) at 1345 BUY GS atLimit $386.39 count=8 $5.49 or 1.4208% above S1
+# 21) at 1315 BUY NVDA atLimit $492 count=5 $16.99 or 3.4532% above S1
+# 22) at 1315 BUY AAPL atLimit $181.68 count=6 $0.84 or 0.4624% above S1
+# 23) at 1315 BUY TSLA atLimit $237.68 count=7 $1.45 or 0.6101% above S1
+# 24) at 1315 BUY INTC atLimit $47.05 count=7 $1.38 or 2.9328% above S1
+# 25) at 1315 SELL VXX atLimit $15.74 count=6 $-0.5 or -3.1687% below R1
+# 26) at 1300 BUY META atLimit $351.47 count=5 $7.19 or 2.0457% above S1
+# 27) at 1200 SELL ROKU atLimit $89.58 count=6 $-0.32 or -0.3547% below R1
+# 28) at 1200 SELL NVDA atLimit $493.32 count=8 $8.38 or 1.6996% above R1
+# (ini.3)  NVDA SELL<<===== above R1  Trade: NVDA LONG_CALLS
+# 29) at 1200 SELL AMD atLimit $140.28 count=8 $2.46 or 1.7504% above R1
+# 30) at 1145 SELL QQQ atLimit $399.05 count=6 $0.51 or 0.1289% above R1
+# 31) at 1145 SELL META atLimit $352.53 count=7 $3.51 or 0.9950% above R1
+# (ini.2)  META SELL<<===== above R1  Trade: META CREDIT_CALL_SPREAD
+# 32) at 1145 SELL MSFT atLimit $371.55 count=7 $-0.1 or -0.0257% below R1
+# 33) at 1145 SELL SPY atLimit $469.99 count=6 $0.18 or 0.0376% above R1
+# 34) at 1145 BUY VXX atLimit $15.53 count=7 $-0.31 or -2.0176% below S1
+# 35) at 1145 SELL GS atLimit $388.75 count=6 $2.71 or 0.6974% above R1
+# 36) at 1130 SELL INTC atLimit $47.65 count=5 $0.06 or 0.1189% above R1
+# 37) at 1130 SELL TSLA atLimit $239.32 count=4 $-1.88 or -0.7860% below R1
+# 38) at 1130 SELL AMZN atLimit $146.29 count=6 $-0.33 or -0.2233% below R1
+# 39) at 1030 BUY TSLA atLimit $238.64 count=4 $2.41 or 1.0113% above S1
+# 40) at 1015 BUY SPY atLimit $468.26 count=4 $2.35 or 0.5019% above S1
+# 41) at 1000 BUY AAPL atLimit $182.17 count=8 $1.33 or 0.7319% above S1
+
+
 def GenerateTrade(arr, idx, arrINIcsv ):
     S1R1str="S1"
     col=colorLimeGreen
