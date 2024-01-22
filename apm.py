ceef15d951f659fccc838d6494c6e2bf62656cc4
algoadmin1 <algoinvestorr@gmail.com>
2024-01-22 13:02:51 -0800

    apm mod

--- apm.py
+++ apm.py
@@ -1,10 +1,19 @@
 #
-# apm.py or Automated Portfolio Manager by John Botti Jan 2024
+#    apm.py -  Automated Portfolio Manager by John Botti Jan 2024
 #
 #
 #
 from portfolio import Portfolio
 
+#
+#  apm should use (1) alphavantage to get HLC for last month, 
+#
+#  apm should use  udates for upcoming earnings for trading strategies, for example:
+#       
+#       EARNINGS_RUNUP_DEBIT_CALL_SPREAD,
+#       EARNINGS_RUNUP_DEBIT_PUT_SPREAD,
+#       BUY_WRITE_WHEEL,
+#
 
 portfolio1 = Portfolio( "Johnnie"  , "354333", "GYFBOB", "etrade", "E3F266FC2A10034D")
 
