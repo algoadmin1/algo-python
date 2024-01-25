0b46a2a1d9058d37c49839b82ae821763278abce
algoadmin1 <algoinvestorr@gmail.com>
2024-01-25 13:01:39 -0800

    watchdog.py tester to buy stock and options working

--- watchdog.py
+++ watchdog.py
@@ -26,6 +26,21 @@ import robin_stocks as rs
 #
 #
 
+def delayLoop(secs):
+    """
+    Delays execution for the specified number of seconds.
+
+    Parameters:
+    - secs (float): The number of seconds to delay.
+
+    Example:
+    >>> delayLoop(5)
+    # Pauses execution for 5 seconds
+    """
+    time.sleep(secs)
+
+# Example usage:
+#delayLoop(3)  
 
 def Check_data(array, datastr):
     return datastr in array
@@ -45,10 +60,66 @@ def printJson(dict0, str0):
     json1 = json.dumps(dict0, indent=10)
     print( str0," json=",json1)
 
+
+def sendOrderSell( buySell, qty0, symbol0 , assettype, priceLimit ):
+    # Get the instrument URL for the stock
+    # instrument = rs.robinhood.get_stock_instrument_data(symbol0)[0]['url']
+    # # Get the current ask price (market price) for the stock
+    # ask_price = rs.robinhood.get_latest_price(symbol0, includeExtendedHours=True, info='ask_price')
+    # # Place a market sell order at  ask price
+    # order_id = rs.robinhood.order_sell_market( instrument=instrument, quantity=qty0 )
+    #priceLimit=300
+    rs.robinhood.order_sell_limit(symbol0 , qty0, priceLimit )
+    print(".robinhood*SELL_LIMIT send0rderSell(" ,buySell, qty0, symbol0 , assettype,") sent to market.")
+
+
+def sendMarketOrder( buySell, qty, symbol0 , assettype ):
+    qtyMAX=10
+    if(assettype=="stock"):
+        qtyMAX=10
+        if(qty>qtyMAX):
+            qty=qtyMAX
+        if(buySell=="BUY"):
+            rs.robinhood.order_buy_market(symbol0, qty)  
+            print(".robinhood*BUY  sendMarket0rder(" ,buySell, qty, symbol0 , assettype,") sent to market.")
+        if(buySell=="SELL"):
+            rs.robinhood.order_sell_market(symbol0, qty)  
+            print(".robinhood*SELL sendMarket0rder(" ,buySell, qty, symbol0 , assettype,") sent to market.")
+
+def sendOptionOrder(symbol0,qty,price0,expdate,strike0,putcall,buysell):
+    #  Buy 5 $150 May 1st, 2020 SPY puts if the price per contract is $1.00. Good until cancelled.
+    # robin_stocks.order_buy_option_limit('open','debit',1.00,'SPY',5,'2020-05-01',150,'put','gtc')
+    # dur='gtc'
+    if(qty==0):
+        print(" Zero qty ==0 , exiting send0ption0rder()")
+        return
+    acct0='497177477'
+    if(buysell=="BUY"):
+        rs.robinhood.order_buy_option_limit('open','debit',price0,symbol0, qty, expdate,strike0,putcall,acct0)
+    if(buysell=="SELL"):
+        rs.robinhood.order_sell_option_limit('close','debit',price0,symbol0, qty, expdate,strike0,putcall,acct0)
+
+
+
+def GetHoldings(str):
+    print("] Your Holdings ",str," :")
+    my_items =  rs.robinhood.build_holdings()
+    h=0;
+    for key,value in my_items.items():
+    # for key,value in my_items():
+        print(h," )")
+        print(key,value)
+        h+=1
+    return my_items
+
+
 # robin_stocks Docs:  https://robin-stocks.readthedocs.io/en/latest/robinhood.html#logging-in-and-out
 def CheckPostionsRobinhood( username0, pwd0 ):
     print("CheckPostionsRobinhood() Positions for ", username0)
-
+    
+    ordersLIVE= 1
+    getOptionsPOSS=0
+    
     days0 = 1
     secsInADay = 86400
     totalseconds = secsInADay * days0
@@ -84,26 +155,105 @@ def CheckPostionsRobinhood( username0, pwd0 ):
     # rs.robinhood.order_buy_market('AMD',2,  "gtc", extendedHours=True )
     # rs.robinhood.order_buy_market(  'AMD',1,timeInForce='gtc', extendedHours=True )
 
-# order(symbol, quantity, "buy", account_number, None, None, timeInForce, extendedHours, jsonify)
-    # rs.robinhood.order_buy_market('AMD', 1) # None , None, "gfc", True )
+    my_items = GetHoldings("BEFORE TRADE")
+
+
+######################################################
+###################
+###################     ORDERS START HERE
+###################
+######################################################
+
+    if(ordersLIVE==1):
+        qty0        =   1
+        sym0        =   "aapl"
+        buySell0= "BUY"
+        assettype0  ="stock"
+        #sendMarketOrder( buySell0, qty0,sym0 , assettype0  )
+            
+        # buySell0    = "SELL"
+        # price0 = 182.50
+        # sendOrderSell( buySell0, qty0, sym0, assettype0, price0 )
+        
+
+        ####################################### 
+        # qty0        =   1
+        # sym0        =   "amd"
+        # putcall     ='call'
+        # price0      =0.98
+        # # price0      =6.30
+        # # expdate="2024-02-02"
+        # expdate     ="2024-01-26"
+        # strike0     =185
+        # buySell0    ="BUY"
+        # # buySell0    ="SELL"
+        # print(".robinhood *SENDING Order" , sym0,qty0,price0,expdate,strike0,putcall, buySell0)
+        # sendOptionOrder(sym0,qty0,price0,expdate,strike0,putcall, buySell0)
+
+        ####################################### 
+        # qty0        =   3
+        # sym0        =   "amd"
+        # putcall     ='call'
+        # price0      =2.70
+        # # price0      =6.30
+        # # expdate="2024-02-02"
+        # expdate     ="2024-01-26"
+        # strike0     =180
+        # buySell0    ="BUY"
+        # # buySell0    ="SELL"
+        # print(".robinhood *SENDING Order" , sym0,qty0,price0,expdate,strike0,putcall, buySell0)
+        # sendOptionOrder(sym0,qty0,price0,expdate,strike0,putcall, buySell0)
+
+
+        qty0        =   0
+        sym0        =   "amzn"
+        putcall     ='call'
+        price0      =2.85
+        # price0      =6.30
+        # expdate="2024-02-02"
+        expdate     ="2024-01-26"
+        strike0     =155
+        buySell0    ="BUY"
+        # buySell0    ="SELL"
+        print(".robinhood *SENDING Order" , sym0,qty0,price0,expdate,strike0,putcall, buySell0)
+        sendOptionOrder(sym0,qty0,price0,expdate,strike0,putcall, buySell0)
+
+
+
+
+
+# POST ORDER SEND   *** DELAY ***
+    secs=2
+    print("delaying ",secs,"seconds...")
+    delayLoop(2.5)  
+    print("resuming...")
+
+
+
+
+
 
 # response:
 # AMD {'price': '179.030000', 'quantity': '0.00000000', 'average_buy_price': '0.0000', 'equity': '0.00', 'percent_change': '0.00', 'intraday_percent_change': '0.00', 'equity_change': '0.000000', 'type': 'stock', 'name': 'AMD', 'id': '940fc3f5-1db5-4fed-b452-f3a2e4562b5f', 'pe_ratio': '1328.230000', 'percentage': '0.00'}
 
 ###### THIS WORKS !
-    my_items = rs.robinhood.options.get_open_option_positions(account_number=None, info=None)
-    printJson(my_items, "Option OPEN Orders")
+    if(getOptionsPOSS==1):
+        my_items = rs.robinhood.options.get_open_option_positions(account_number=None, info=None)
+        printJson(my_items, "Option OPEN Orders")
+
 
 
 ###### THIS WORKS !
-    print("] calling build_holdings ")
-    my_items =  rs.robinhood.build_holdings()
-    h=0;
-    for key,value in my_items.items():
-    # for key,value in my_items():
-        print(h," )")
-        print(key,value)
-        h+=1
+    my_items = GetHoldings("AFTER TRADE")
+
+    # print("] Holdings AFTER BUY ")
+    # my_items =  rs.robinhood.build_holdings()
+    # h=0;
+    # for key,value in my_items.items():
+    # # for key,value in my_items():
+    #     print(h," )")
+    #     print(key,value)
+    #     h+=1
 
     # rs.login(username=username0, password=pwd0)
     # portfolio = rs.account.get_portfolio()
@@ -144,8 +294,12 @@ CheckPostionsRobinhood( "roguequant1@gmail.com", pwd0 )
 
 # Starting Balance
 startingBalance_default = float(100000.0)
-print("\nEnter Starting Balance (", startingBalance_default, "):")
+print("\nEnter Starting Balance (", startingBalance_default, "  [0=exit]):")
 startingBalance = input()
+if startingBalance == "-":
+    exit("exiting...-")
+if startingBalance == "0":
+    exit("exiting...0")
 if startingBalance == "":
     print("  Defaulting Starting Balance to ", startingBalance_default)
     startingBalance = startingBalance_default
