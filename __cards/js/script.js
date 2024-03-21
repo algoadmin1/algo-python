c9f59d0580aa73d9f6a7fc6182e3539c421b9124
algoadmin1 <algoinvestorr@gmail.com>
2024-03-21 02:57:41 -0700

    XYZ

--- __cards/js/script.js
+++ __cards/js/script.js
@@ -1,19 +1,35 @@
 // script.js
 // let johnie=1;
 
-var swiper = new Swiper(".slide-content", {
-            slidesPerView: 3,
-            spaceBetween: 30,
-            slidesPerGroup: 3,
-            loop: true,
-            loopFillGroupWithBlank: true,
-            pagination:{
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-            navigation:{
-                nextEl: ".swiper-button-next", 
-                prevEl: ".swiper-button-prev", 
-            },
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Swiper 
+// var swiper = new Swiper(".slide-content", {
+//             slidesPerView: 3,
+//             spaceBetween: 30,
+//             slidesPerGroup: 3,
+//             loop: true,
+//             loopFillGroupWithBlank: true,
+//             pagination:{
+//                 el: ".swiper-pagination",
+//                 clickable: true,
+//             },
+//             navigation:{
+//                 nextEl: ".swiper-button-next", 
+//                 prevEl: ".swiper-button-prev", 
+//             },
+
+// });
