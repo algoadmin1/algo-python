2f708ad704c880425e7a2901ba03afc29081d6b3
algoadmin1 <algoinvestorr@gmail.com>
2024-06-14 18:24:33 -0700

    video hero html css

--- ____algoz_ai/algoz_ai_login-register-main/database.php
+++ ____algoz_ai/algoz_ai_login-register-main/database.php
@@ -1,9 +1,14 @@
 <?php
+// hit this link for full tutorial
+$youtubeUrl="https://youtu.be/9_kQ2bjqSzk?si=vLISSejcsXVqQNVc";
 
-$hostName = "localhost";
-$dbUser = "root";
-$dbPassword = "";
-$dbName = "login_register";
+$hostName = "localhost";  
+$website="https://algoz.ai";
+$dbUser="u184668114_algozai";
+ $dbUserOrig = "root";
+$dbPassword = "Vegas202";
+$dbName = "u184668114_users";
+$dbPassword.="4"."!";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("Something went wrong;");
